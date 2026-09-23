a34b8e079be4aa024dcd706be5ab30d16772dd32
Wakiljnr <120278456+Wakiljnr@users.noreply.github.com>
2026-09-23 20:07:58 +0100

    Fix database connection conflict

--- db.php
+++ db.php
@@ -1,20 +1,4 @@
 <?php
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$password = getenv("DB_PASS");
-$database = getenv("DB_NAME");
-$port = getenv("DB_PORT") ?: 3306;
-
-$conn = new mysqli(
-    $host,
-    $user,
-    $password,
-    $database,
-    $port
-);
-
-=======
-/* Database connection for Render */
 
 $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
@@ -22,7 +6,6 @@ $password = getenv("DB_PASSWORD");
 $database = getenv("DB_NAME");
 $port = getenv("DB_PORT") ?: 3306;
 
-// Create MySQL connection
 $conn = new mysqli(
     $host,
     $user,
@@ -31,11 +14,10 @@ $conn = new mysqli(
     $port
 );
 
-// Check connection
->>>>>>> 8ca110764e3b267a6003318296766200a842cb24
 if ($conn->connect_error) {
     die("Database connection failed.");
 }
 
 $conn->set_charset("utf8mb4");
+
 ?>
